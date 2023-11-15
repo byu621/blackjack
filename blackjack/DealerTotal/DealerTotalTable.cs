28ad10d5ed5c3ee0cc171725404aa7e9e3771045
Benjamin Yu <byu621@aucklanduni.ac.nz>
2023-11-15 18:03:37 +1300

    add soft values to dealer total table

--- blackjack/DealerTotal/DealerTotalTable.cs
+++ blackjack/DealerTotal/DealerTotalTable.cs
@@ -21,30 +21,97 @@ public class DealerTotalTable
         _dictionary.Add(new Hand(HARD, 18), new(new Hand(HARD, 18), Zero, One, Zero, Zero, Zero, Zero));
         _dictionary.Add(new Hand(HARD, 17), new(new Hand(HARD, 17), One, Zero, Zero, Zero, Zero, Zero));
 
-        // possible increases to a hand's total is 1..10
-        for (int startingHand = 16; startingHand >= 2; startingHand--)
+        _dictionary.Add(new Hand(SOFT, 21), new(new Hand(SOFT, 21), Zero, Zero, Zero, Zero, One, Zero));
+        _dictionary.Add(new Hand(SOFT, 20), new(new Hand(SOFT, 20), Zero, Zero, Zero, One, Zero, Zero));
+        _dictionary.Add(new Hand(SOFT, 19), new(new Hand(SOFT, 19), Zero, Zero, One, Zero, Zero, Zero));
+        _dictionary.Add(new Hand(SOFT, 18), new(new Hand(SOFT, 18), Zero, One, Zero, Zero, Zero, Zero));
+        _dictionary.Add(new Hand(SOFT, 17), new(new Hand(SOFT, 17), One, Zero, Zero, Zero, Zero, Zero));
+
+        for (int total = 16; total >= 2; total--)
         {
-            Probability p17 = Zero;
-            Probability p18 = Zero;
-            Probability p19 = Zero;
-            Probability p20 = Zero;
-            Probability p21 = Zero;
-            Probability pBust = Zero;
-            for (int hitCard = 1; hitCard <= 10; hitCard++)
-            {
-                int handTotal = startingHand + hitCard;
-                Probability handTotalProbability = hitCard == 10 ? FourThirteenth : OneThirteenth;
-
-                p17 += _dictionary.GetValueOrDefault(new Hand(HARD, handTotal), HARD_22).P17 * handTotalProbability;
-                p18 += _dictionary.GetValueOrDefault(new Hand(HARD, handTotal), HARD_22).P18 * handTotalProbability;
-                p19 += _dictionary.GetValueOrDefault(new Hand(HARD, handTotal), HARD_22).P19 * handTotalProbability;
-                p20 += _dictionary.GetValueOrDefault(new Hand(HARD, handTotal), HARD_22).P20 * handTotalProbability;
-                p21 += _dictionary.GetValueOrDefault(new Hand(HARD, handTotal), HARD_22).P21 * handTotalProbability;
-                pBust += _dictionary.GetValueOrDefault(new Hand(HARD, handTotal), HARD_22).PBust * handTotalProbability;
-            }
-
-            _dictionary.Add(new Hand(HARD, startingHand), new(new Hand(HARD, startingHand), p17, p18, p19, p20, p21, pBust));
+            _dictionary.Add(new Hand(HARD, total), ComputeHard(total));
         }
+    }
+
+    private DealerTotalProbability ComputeHard(int total)
+    {
+        if (total <= 1)
+        {
+            throw new ArgumentException($"total is invalid total = {total}");
+        }
+
+        if (total >= 22)
+        {
+            return HARD_22;
+        }
+
+        if (_dictionary.ContainsKey(new Hand(HARD, total)))
+        {
+            return _dictionary[new Hand(HARD, total)];
+        }
+
+        Probability p17 = Zero;
+        Probability p18 = Zero;
+        Probability p19 = Zero;
+        Probability p20 = Zero;
+        Probability p21 = Zero;
+        Probability pBust = Zero;
+        DealerTotalProbability dealerTotalProbability;
+        for (int hitCard = 1; hitCard <= 10; hitCard++)
+        {
+            Probability probability = hitCard == 10 ? FourThirteenth : OneThirteenth;
+            dealerTotalProbability = hitCard == 1 ? ComputeSoft(total + 11) : ComputeHard(total + hitCard);
+
+            p17 += dealerTotalProbability.P17 * probability;
+            p18 += dealerTotalProbability.P18 * probability;
+            p19 += dealerTotalProbability.P19 * probability;
+            p20 += dealerTotalProbability.P20 * probability;
+            p21 += dealerTotalProbability.P21 * probability;
+            pBust += dealerTotalProbability.PBust * probability;
+        }
+
+        return new(new Hand(HARD, total), p17, p18, p19, p20, p21, pBust);
+    }
+
+    private DealerTotalProbability ComputeSoft(int total)
+    {
+        if (total <= 0)
+        {
+            throw new ArgumentOutOfRangeException($"total is invalid total = {total}");
+        }
+
+        if (total >= 22)
+        {
+            return ComputeHard(total - 10);
+        }
+
+        if (_dictionary.ContainsKey(new Hand(SOFT, total)))
+        {
+            return _dictionary[new Hand(SOFT, total)];
+        }
+
+        Probability p17 = Zero;
+        Probability p18 = Zero;
+        Probability p19 = Zero;
+        Probability p20 = Zero;
+        Probability p21 = Zero;
+        Probability pBust = Zero;
+        DealerTotalProbability dealerTotalProbability;
+        for (int hitCard = 1; hitCard <= 10; hitCard++)
+        {
+            Probability probability = hitCard == 10 ? FourThirteenth : OneThirteenth;
+            dealerTotalProbability = ComputeSoft(total + hitCard);
+
+            p17 += dealerTotalProbability.P17 * probability;
+            p18 += dealerTotalProbability.P18 * probability;
+            p19 += dealerTotalProbability.P19 * probability;
+            p20 += dealerTotalProbability.P20 * probability;
+            p21 += dealerTotalProbability.P21 * probability;
+            pBust += dealerTotalProbability.PBust * probability;
+        }
+
+        _dictionary.Add(new Hand(SOFT, total), new(new Hand(SOFT, total), p17, p18, p19, p20, p21, pBust));
+        return _dictionary[new Hand(SOFT, total)];
     }
 
     public void WriteToCsv()
