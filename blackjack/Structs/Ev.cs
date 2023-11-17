6fa9f7c1c35f6022161e01f075eaf951de6d0ef0
Benjamin Yu <byu621@aucklanduni.ac.nz>
2023-11-17 18:36:38 +1300

    add new struct for ev, expected value, -1 means guaranteed loss of one betting unit

---
+++ blackjack/Structs/Ev.cs
@@ -0,0 +1,22 @@
+﻿namespace blackjack;
+
+public record Ev
+{
+    public decimal Value { get; }
+
+    public Ev(decimal value)
+    {
+        Value = value;
+    }
+
+    public static Ev Calc(Probability win, Probability lose)
+    {
+        return new Ev(win.Value - lose.Value);
+    }
+
+    public override string ToString()
+    {
+        decimal percentage = Value * 100;
+        return $"{percentage.ToString("0.00")}%";
+    }
+}
