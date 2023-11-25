fef68d65ffad28dcb89992fa783da8370dc39d26
Benjamin-Yu <ben.yu2@datacom.co.nz>
2023-11-26 12:19:14 +1300

    update program.cs

--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -4,8 +4,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // DealerTotalRunnner.WriteToCsv();
-        PlayerHitRunner.Print();
         PlayerHitRunner.WriteToCsv();
         PlayerHitStandRunner.WriteToCsv();
         PlayerDoubleRunner.WriteToCsv();
