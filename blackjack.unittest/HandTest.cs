24b63d833415d5caa0b194eaee828004ba80fca0
Benjamin Yu <byu621@aucklanduni.ac.nz>
2023-12-05 17:06:17 +1300

    reorganize tests

--- blackjack.unittest/HandTest.cs
+++ blackjack.unittest/HandTest.cs
@@ -1,6 +1,6 @@
 namespace blackjack.unittest;
 
-public class UnitTest1
+public class HandTest
 {
     [Fact]
     public void TestHandConstructor()
@@ -41,20 +41,12 @@ public class UnitTest1
         Assert.Equal(Shape.SOFT, hand.Shape);
         Assert.True(hand.Blackjack);
     }
-
+    
     [Fact]
     public void TestUpCard()
     {
         Hand hand = new Hand();
         (hand, _) = hand.Hit(new(1));
         Assert.Equal(1, hand.UpCard!.Value);
-    }
-
-    [Fact]
-    public void TestShoeDeal()
-    {
-        Shoe shoe = new Shoe(6);
-        Hand hand = shoe.Deal();
-        Assert.NotNull(hand.UpCard);
     }
 }
---
+++ blackjack.unittest/ShoeTest.cs
@@ -0,0 +1,12 @@
+namespace blackjack.unittest;
+
+public class ShoeTest
+{
+    [Fact]
+    public void TestShoeDeal()
+    {
+        Shoe shoe = new Shoe(6);
+        Hand hand = shoe.Deal();
+        Assert.NotNull(hand.UpCard);
+    }
+}
