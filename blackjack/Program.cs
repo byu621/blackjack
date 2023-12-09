33a292704f7091b2b0c6704827216d962d294de7
Benjamin-Yu <ben.yu2@datacom.co.nz>
2023-12-09 15:41:48 +1300

    write standtable to csv

--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -5,25 +5,26 @@ internal static class Program
     private static void Main(string[] args)
     {
         int numDecksInShoe = 6;
-        int numShoe = 10000;
+        int numShoe = 1000000;
         int penetration = 80;
 
         SimulationStand simulationStand = new(numDecksInShoe, penetration);
         (decimal standEv, StandTable standTable) = simulationStand.SimulateStand(numShoe);
+        standTable.WriteToFile();
 
         Console.WriteLine(standTable.ToString());
         Console.WriteLine((standEv * 100).ToString("0.00") + "%");
 
-        SimulationHit simulationHit = new(numDecksInShoe, penetration, standTable);
-        (decimal hitEv, HitTable hitTable) = simulationHit.SimulateHit(numShoe);
-        
-        Console.WriteLine(hitTable.ToString());
-        Console.WriteLine((hitEv * 100).ToString("0.00") + "%");
-
-        SimulationDouble simulationDouble = new(numDecksInShoe, penetration, standTable);
-        (decimal doubleEv, HitTable doubleTable) = simulationDouble.SimulateDouble(numShoe);
-
-        Console.WriteLine(doubleTable.ToString());
-        Console.WriteLine((doubleEv * 100).ToString("0.00") + "%");
+        // SimulationHit simulationHit = new(numDecksInShoe, penetration, standTable);
+        // (decimal hitEv, HitTable hitTable) = simulationHit.SimulateHit(numShoe);
+        //
+        // Console.WriteLine(hitTable.ToString());
+        // Console.WriteLine((hitEv * 100).ToString("0.00") + "%");
+        //
+        // SimulationDouble simulationDouble = new(numDecksInShoe, penetration, standTable);
+        // (decimal doubleEv, HitTable doubleTable) = simulationDouble.SimulateDouble(numShoe);
+        //
+        // Console.WriteLine(doubleTable.ToString());
+        // Console.WriteLine((doubleEv * 100).ToString("0.00") + "%");
     }
 }
--- blackjack/table/StandTable.cs
+++ blackjack/table/StandTable.cs
@@ -4,15 +4,15 @@ namespace blackjack;
 
 public class StandTable
 {
-    private string[] headers = {"A", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
-    private string[] rows = {"20", "19", "18", "17", "16"}; 
-    private decimal[,] table;
-    private int[,] count;
+    private readonly string[] _headers = {"A", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+    private readonly string[] _rows = {"20", "19", "18", "17", "16"}; 
+    private readonly decimal[,] _table;
+    private readonly int[,] _count;
 
     public StandTable()
     {
-        table = new decimal[rows.Length, headers.Length];
-        count = new int[rows.Length, headers.Length];
+        _table = new decimal[_rows.Length, _headers.Length];
+        _count = new int[_rows.Length, _headers.Length];
     }
 
     public void Add(Card dealerCard, Hand player, decimal ev)
@@ -20,42 +20,48 @@ public class StandTable
         if (player.Value == 21) throw new ArgumentException();
         int column = dealerCard.Value == 1 ? 0 : 11 - dealerCard.Value;
         int row = player.Value <= 16 ? 4 : 20 - player.Value;
-        table[row, column] += ev; 
-        count[row, column] += 1;
+        _table[row, column] += ev; 
+        _count[row, column] += 1;
     }
     
     public decimal Get(Card dealerCard, Hand player)
     {
         int column = dealerCard.Value == 1 ? 0 : 11 - dealerCard.Value;
         int row = player.Value <= 16 ? 4 : 20 - player.Value;
-        return count[row, column] == 0 ? table[row, column] : table[row, column] / count[row, column];
+        return _count[row, column] == 0 ? _table[row, column] : _table[row, column] / _count[row, column];
     }
 
     public override string ToString()
     {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.Append("#");
-        foreach(string header in headers)
+        foreach(string header in _headers)
         {
             stringBuilder.Append($",{header}");
         }
-        for (int i = 0; i < rows.Length; i++)
+        for (int i = 0; i < _rows.Length; i++)
         {
             stringBuilder.AppendLine();
-            string row = rows[i];
-            string ace = (table[i,0] / count[i,0] * 100).ToString("0.00") + "%";
-            string ten = (table[i,1] / count[i,1] * 100).ToString("0.00") + "%";
-            string nine = (table[i,2] / count[i,2] * 100).ToString("0.00") + "%";
-            string eight = (table[i,3] / count[i,3] * 100).ToString("0.00") + "%";
-            string seven = (table[i,4] / count[i,4] * 100).ToString("0.00") + "%";
-            string six = (table[i,5] / count[i,5] * 100).ToString("0.00") + "%";
-            string five = (table[i,6] / count[i,6] * 100).ToString("0.00") + "%";
-            string four = (table[i,7] / count[i,7] * 100).ToString("0.00") + "%";
-            string three = (table[i,8] / count[i,8] * 100).ToString("0.00") + "%";
-            string two = (table[i,9] / count[i,9] * 100).ToString("0.00") + "%";
+            string row = _rows[i];
+            string ace = (_table[i,0] / _count[i,0] * 100).ToString("0.00") + "%";
+            string ten = (_table[i,1] / _count[i,1] * 100).ToString("0.00") + "%";
+            string nine = (_table[i,2] / _count[i,2] * 100).ToString("0.00") + "%";
+            string eight = (_table[i,3] / _count[i,3] * 100).ToString("0.00") + "%";
+            string seven = (_table[i,4] / _count[i,4] * 100).ToString("0.00") + "%";
+            string six = (_table[i,5] / _count[i,5] * 100).ToString("0.00") + "%";
+            string five = (_table[i,6] / _count[i,6] * 100).ToString("0.00") + "%";
+            string four = (_table[i,7] / _count[i,7] * 100).ToString("0.00") + "%";
+            string three = (_table[i,8] / _count[i,8] * 100).ToString("0.00") + "%";
+            string two = (_table[i,9] / _count[i,9] * 100).ToString("0.00") + "%";
             stringBuilder.Append($"{row},{ace},{ten},{nine},{eight},{seven},{six},{five},{four},{three},{two}");
         }
 
         return stringBuilder.ToString();
+    }
+    
+    public void WriteToFile()
+    {
+        using StreamWriter writer = new StreamWriter("data/stand.csv", false);
+        writer.Write(ToString());
     }
 }
