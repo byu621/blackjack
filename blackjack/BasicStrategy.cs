965926cefcde6aefcca1850bf96c990ba32a7dd8
Benjamin-Yu <ben.yu2@datacom.co.nz>
2023-12-16 14:00:18 +1300

    load pairbasicstrategy

--- blackjack/BasicStrategy.cs
+++ blackjack/BasicStrategy.cs
@@ -3,6 +3,7 @@
 public class BasicStrategy
 {
     private readonly Dictionary<State, Action> _dict = new();
+    private readonly Dictionary<PairState, Action> _pairDict = new();
 
     private readonly Dictionary<string, Action> _actionDict = new()
     {
@@ -16,6 +17,7 @@ public class BasicStrategy
     {
         Load("HardBasicStrategy.csv", Shape.Hard);
         Load("SoftBasicStrategy.csv", Shape.Soft);
+        LoadPair("PairBasicStrategy.csv");
     }
 
     public Action GetAction(State state)
@@ -44,6 +46,27 @@ public class BasicStrategy
                 State state = new(shape, value, dealer);
                 Action action = _actionDict[split[j]];
                 _dict[state] = action;
+            }
+        }
+    }
+    
+    private void LoadPair(string fileName)
+    {
+        string[] lines = File.ReadAllLines(fileName);
+        int[] dealerValues = lines[0][2..].Split(",").Select(x => x == "A" ? 1 : int.Parse(x)).ToArray();
+        for (var i = 1; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            string[] split = line.Split(",");
+            int value = split[0] == "A" ? 1 : int.Parse(split[0]);
+            Card card = new(value);
+
+            for (int j = 1; j < split.Length; j++)
+            {
+                int dealer = dealerValues[j - 1];
+                PairState state = new(card, dealer);
+                Action action = _actionDict[split[j]];
+                _pairDict[state] = action;
             }
         }
     }
--- blackjack/State.cs
+++ blackjack/State.cs
@@ -6,3 +6,9 @@ public struct State(Shape shape, int value, int dealer)
     public int Value = value;
     public int Dealer = dealer;
 }
+
+public struct PairState(Card card, int dealer)
+{
+    public Card Card = card;
+    public int Dealer = dealer;
+}
