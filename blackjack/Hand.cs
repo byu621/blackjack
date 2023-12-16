3ea8bd2adb50d80c495dce4a55d9e17fd0690db5
Benjamin-Yu <ben.yu2@datacom.co.nz>
2023-12-16 14:48:46 +1300

    fix hand

--- blackjack/Hand.cs
+++ blackjack/Hand.cs
@@ -2,10 +2,12 @@ namespace blackjack;
 
 public record Hand
 {
-    public int Value => _cards.Sum(card => card.Value);
-    public Shape Shape => Shape.Hard;
-    public bool Blackjack => _cards.Count == 2 && Value == 21;
-    public bool IsBust => Value > 21;
+    private int HardValue => _cards.Sum(card => card.Value);
+    private int SoftValue => HasAce ? HardValue + 10 : HardValue;
+    private Shape Shape => HasAce && SoftValue <= 21 ? Shape.Soft : Shape.Hard;
+    public bool Blackjack => _cards.Count == 2 && SoftValue == 21;
+    public bool IsBust => HardValue > 21;
+    private bool HasAce => _cards.Any(c => c.Value == 1);
 
     private readonly List<Card> _cards;
 
